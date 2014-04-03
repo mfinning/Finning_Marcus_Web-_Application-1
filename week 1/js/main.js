0c0a48ff3301449b3336786f8e3490e3da2e7f48
mfinning <mfinning@gmail.com>
2014-04-03 13:34:46 -0600

    added function and variables

--- week 1/js/main.js
+++ week 1/js/main.js
@@ -1,3 +1,48 @@
 /**
- * Created by RAC on 4/3/14.
+ * Created by Marcus Finning on 4/2/14.
+ * week 1
+ * The Dual
  */
+//self exc/
+
+(function(){
+
+   console.log("it works so far");
+
+
+    //players
+
+    var playerOneName = "Hawkeye";
+    var playerTwoName = "Captenamrica";
+
+    //DMG
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //life
+
+    var playweOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //counter
+
+    var round=1;
+
+    function fight(){
+    console.log("the fight");
+        for (var i=0;i<10;i++){
+            console.log(i);
+        };
+    };
+
+    //winchk
+
+    function winnerCheck(){
+
+    };
+
+// start
+    console.log("program start");
+    fight();
+
+})();
