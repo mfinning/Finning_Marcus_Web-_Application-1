621ba00cf7bc0c2fb0752325c2ac235083930864
mfinning <mfinning@gmail.com>
2014-04-03 15:07:49 -0600

    debug
    was getting the msg undefined function
    corrected
    not to self dbl chk caps were needed

--- week 1/js/main.js
+++ week 1/js/main.js
@@ -40,15 +40,20 @@
          //dmg formula Math.floor(Math.random()*(max - min) + min);
             var minDamage1 = player1Damage *.5;
             var minDamage2 = player2Damage *.5;
-            var f1 = Math.floor(math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(math.random()*(player2Damage-minDamage2)+minDamage2);
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
 
-            console.log(f1);
-            console.log(f2);
+            //console.log(f1);
+            //console.log(f2);
 
             //dmg inflicted
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
 
-          play
+            console.log(playerOneHealth);
+            console.log(playerTwoHealth);
+
+
         };
     };
 
