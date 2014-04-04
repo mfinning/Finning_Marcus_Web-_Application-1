8d867545f4b3362c6baa9fbdac46c8059981a1d6
mfinning <mfinning@gmail.com>
2014-04-03 18:07:04 -0600

    Revert 538b284..98c84d7
    This rolls back to commit 538b2840bc5eb72b5a2dcf6c6d3089093e65d023.

--- week 1/Finning_Marcus wk1_part2_The_Dule/js/main.js
+++ week 1/Finning_Marcus wk1_part2_The_Dule/js/main.js
@@ -12,14 +12,17 @@
 
     //players
 
-    var fighter1 = ["Hawkeye",20, 100];
-    var fighter2 = ["CaptenAmrica",20,100];
+    var playerOneName = "Hawkeye";
+    var playerTwoName = "CaptenAmrica";
 
     //DMG
-    //var player1Damage = 20;
-    //var player2Damage = 20;
-    // var playerOneHealth = 100;
-    //var playerTwoHealth = 100;
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //life
+
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
 
     //counter
 
@@ -28,34 +31,34 @@
     function fight(){
 
     //console.log("the fight");
-    alert(fighter1+":"+fighter1+" * Fight * "+fighter2+":"+fighter2);
+    alert(playerOneName+":"+playerOneHealth+" * Fight * "+playerTwoName+":"+playerTwoHealth);
 
         for (var i=0;i<10;i++)
         {
       //dmg formula Math.floor(Math.random()*(max - min) + min);
-            var minDamage1 = fighter1 *.5;
-            var minDamage2 = fighter2 *.5;
-            var f1 = Math.floor(Math.random()*(fighter1-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(fighter2-minDamage2)+minDamage2);
+            var minDamage1 = player1Damage *.5;
+            var minDamage2 = player2Damage *.5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
 
             //console.log(f1);
             //console.log(f2);
 
             //dmg inflicted
-            fighter1-=f1;
-            fighter2-=f2;
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
 
             //console.log(playerOneHealth);
             //console.log(playerTwoHealth);
 
-            console.log(fighter1+":"+fighter2+" "+fighter2+":"+fighter1);
+            console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
 
             var result = winnerCheck();
             console.log(result);
             if (result ==="no winner")
             {
                 round++;
-                alert(fighter1+":"+fighter1+" *ROUND "+round+" OVER"+"* "+fighter2+":"+fighter2);
+                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" OVER"+"* "+playerTwoName+":"+playerTwoHealth);
             } else{
                 alert(result);
                 break;
@@ -72,14 +75,14 @@
         var result="no winner";
         console.log(result);
 
-        if(fighter1<1 && fighter2<1)
+        if(playerOneHealth<1 && playerTwoHealth<1)
         {
             result = "no survivers";
-          }else if(fighter1<1){
-            result = fighter2+" is The Chapinon"
-          }else if(fighter2<1)
+          }else if(playerOneHealth<1){
+            result = playerTwoName+" is The Chapinon"
+          }else if(playerTwoHealth<1)
         {
-            result = fighter1+" is The Chapinon"
+            result = playerOneName+" is The Chapinon"
         }
         return result;
        };
